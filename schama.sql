4a889afbb5151e3112d24377ddcd7156c30d2b8a
aniketlahase54 <aniketlahase122@gmail.com>
2026-07-05 15:49:11 +0530

    Initial commit: SQL PROJECT

---
+++ schama.sql
@@ -0,0 +1,100 @@
+CREATE DATABASE olist_db;
+USE olist_db;
+
+
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state CHAR(2)
+);
+
+CREATE TABLE geolocation (
+    geolocation_zip_code_prefix INT,
+    geolocation_lat DECIMAL(10 , 8 ),
+    geolocation_lng DECIMAL(11 , 8 ),
+    geolocation_city VARCHAR(100),
+    geolocation_state CHAR(2)
+);
+
+CREATE TABLE sellers (
+    seller_id VARCHAR(50) PRIMARY KEY,
+    seller_zip_code_prefix INT,
+    seller_city VARCHAR(100),
+    seller_state CHAR(2)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_lenght INT,
+    product_description_lenght INT,
+    product_pordershotos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(30),
+    order_purchase_timestamp DATETIME NULL,
+    order_approved_at DATETIME NULL,
+    order_delivered_carrier_date DATETIME NULL,
+    order_delivered_customer_date DATETIME NULL,
+    order_estimated_delivery_date DATETIME NULL,
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id)
+);
+
+
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    order_item_id INT,
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    shipping_limit_date DATETIME,
+    price DECIMAL(10 , 2 ),
+    freight_value DECIMAL(10 , 2 ),
+    PRIMARY KEY (order_id , order_item_id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id),
+    FOREIGN KEY (product_id)
+        REFERENCES products (product_id),
+    FOREIGN KEY (seller_id)
+        REFERENCES sellers (seller_id)
+);
+
+
+CREATE TABLE order_payments (
+    order_id VARCHAR(50),
+    payment_sequential INT,
+    payment_type VARCHAR(30),
+    payment_installments INT,
+    payment_value DECIMAL(10 , 2 ),
+    PRIMARY KEY (order_id , payment_sequential),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id)
+);
+
+
+CREATE TABLE order_reviews (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50),
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date DATETIME,
+    review_answer_timestamp DATETIME,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (order_id)
+);
+
+CREATE TABLE product_category_translation (
+    product_category_name VARCHAR(100) PRIMARY KEY,
+    product_category_name_english VARCHAR(100)
+);
